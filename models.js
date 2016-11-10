4dc31e7ef16813c58547f06b65f4c0dc2249a67e
bitonator <maha@radioflote.com>
2016-11-10 20:31:03 +0530

    working model objects

--- models.js
+++ models.js
@@ -5,16 +5,16 @@ blue.models={}
 blue.M={}
 
 blue.core.Model=function() {
-    this.blue={};
-    this.blue.primaryKey="id";
-    this[this.blue.primaryKey]=null;
+    this.__blue__={};
+    this.__blue__.primaryKey="id";
+    this[this.__blue__.primaryKey]=null;
 }
 blue.core.ModelPrototype={
     setResource: function(resource) {
-        this.blue.resource=resource;
+        this.__blue__.resource=resource;
     },
     save: function() {
-        if(this[this.blue.primaryKey]==null) {
+        if(this[this.__blue__.primaryKey]==null) {
             console.log("Model create not implemented yet!");
         } else {
             console.log("Model update not implemented yet!");
@@ -23,23 +23,58 @@ blue.core.ModelPrototype={
 }
 blue.core.Model.prototype=blue.core.ModelPrototype;
 
+blue.createModel=function(modelname, prototype) {
+    blue.M[modelname]=function() {
+        blue.core.Model.call();
+    }
+    blue.M[modelname].prototype=new blue.core.Model();
+}
+
 blue.core.Resource=function(keys) {
-    this.blue={};
+    this.__blue__={};
 
     for(var key in keys) {
         if(key.startsWith("blue_")) {
             attr=key.substring(5);
-            this.blue[attr]=keys[key];
+            this.__blue__[attr]=keys[key];
         } else {
             this[key]=keys[key];
         }
     }
+
+    this.preprocess();
 }
 blue.core.ResourcePrototype={
+    preprocess: function() {
+        this.__blue__.keys=[];
+        this.__blue__.parsedAttributes=[];
+        this.__blue__.preparedAttributes=[];
+        for(var index in this.attributes) {
+            attr=this.attributes[index];
+            this.__blue__.keys.push(attr);
+        }
+
+        for(var key in this) {
+            if(key.startsWith("parse_")) {
+                attr=key.substring(6);
+                this.__blue__.keys.push(attr);
+                this.__blue__.parsedAttributes.push(attr);
+            }
+            if(key.startsWith("prepare_")) {
+                this.__blue__.preparedAttributes.push(key.substring(8));
+            }
+        }
+
+        for(var key in this.paths) {
+            this.__blue__.keys.push(key); 
+        }
+
+        for(var key in this.models) {
+            this.__blue__.keys.push(key);
+        }
+    },
     parse: function(data) {
-        console.log("Blue is ");
-        console.log(this.blue);
-        var object=new this.blue.model();
+        var object=new this.__blue__.model();
         object.setResource(this);
         
         for(var index in this.attributes) {
@@ -47,11 +82,9 @@ blue.core.ResourcePrototype={
             object[key]=data[key];
         }
 
-        for(var key in this) {
-            if(key.startsWith("parse_")) {
-                attr=key.substring(6);
-                object[attr]=this[key](data);
-            }
+        for(var index in this.__blue__.parsedAttributes) {
+            key=this.__blue__.parsedAttributes[index];
+            object[key]=this["parse_"+key](data);
         }
 
         for(var key in this.paths) {
@@ -74,37 +107,29 @@ blue.core.ResourcePrototype={
 
     prepare:function(model) {
         var object={};
-        for(var key in this.blue.keys) {
-            attr=this.blue.keys[key];
+        for(var key in this.__blue__.keys) {
+            attr=this.__blue__.keys[key];
             if(this.read_only.indexOf(attr)==-1) {
-                var prepare_fn="prepare_"+attr;
-                if(prepare_fn in this) {
-                    object[attr]=this[prepare_fn](model);
+                if(this.__blue__.preparedAttributes.indexOf(attr)!=-1) {
+                    object[attr]= this["prepare_"+attr](model);
                 } else {
                     object[attr]=model[attr];
                 }
-            }         
+            } 
         }
         return object;
     }
 }
 blue.core.Resource.prototype=blue.core.ResourcePrototype;
 
-### test code
-blue.M.board=function() {
-    blue.core.Model.call(this);
-};
-blue.M.board.prototype=new blue.core.Model();
+//// Test Code
+blue.createModel("board", {});
 blue.R.Board=new blue.core.Resource({
     blue_model: blue.M.board,
     attributes: ["id", "name"]
 });
 
-blue.M.user=function() {
-    blue.core.Model.call(this);
-};
-blue.M.user.prototype=new blue.core.Model();
-
+blue.createModel("user", {});
 blue.R.User=new blue.core.Resource({
     blue_model: blue.M.user,
     attributes: ["first_name", "last_name"],
